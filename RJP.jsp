2207ec7c0d61ea6ad85e24c7831b7d7dead5825f
RIDOY MD MONIR HOSSAIN <mr25304046@ga.ttc.ac.jp>
2025-12-05 15:21:16 +0900

    A finish

---
+++ RJP.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html; charset=utf-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ユーザー登録</title>
+</head>
+<body>
+    <h1>ユーザー登録</h1>
+    <form method="post" action="./registtest"> 
+        ユーザー名<input type="text" name="name"><br>
+        好きなゲーム<input type="text" name="fav"><br>
+        <input type="submit" value="登録" />
+    </form>
+</body>
+</html>
---
+++ WEB-INF/src/RegistTestServlet.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class RegistTestServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        req.setCharacterEncoding("utf-8");
+
+        String username = req.getParameter("name");
+        String password = req.getParameter("fav");
+
+        req.setAttribute("name", username);
+        req.setAttribute("fav", favoritegame);
+
+        RequestDispatcher dispatcher =
+                req.getRequestDispatcher("output");
+
+        dispatcher.forward(req, res);
+    }
+}
