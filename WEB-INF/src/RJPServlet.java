2cfbba84740e743e737d98027a20926b78a9dc03
RIDOY MD MONIR HOSSAIN <mr25304046@ga.ttc.ac.jp>
2025-12-05 16:05:11 +0900

    jikko

--- WEB-INF/src/RJPServlet.java
+++ WEB-INF/src/RJPServlet.java
@@ -6,22 +6,34 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class RegistTestServlet extends HttpServlet {
+public class RJPServlet extends HttpServlet {
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
 
-        req.setCharacterEncoding("utf-8");
+        // 文字コードの指定
+        req.setCharacterEncoding("UTF-8");
 
-        String username = req.getParameter("name");
+        // 入力JSPから送られてきた値を取得
+        String username     = req.getParameter("name");
         String favoritegame = req.getParameter("fav");
 
+        // 出力JSPで使うために属性として保存
         req.setAttribute("name", username);
         req.setAttribute("fav", favoritegame);
 
+        // 出力JSP（output.jsp）へフォワード
         RequestDispatcher dispatcher =
-                req.getRequestDispatcher("output");
+                req.getRequestDispatcher("output.jsp");
 
         dispatcher.forward(req, res);
+    }
+
+    // GETで来た場合もPOSTと同じ処理
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        doPost(req, res);
     }
 }
